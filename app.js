4c7438a4cdcc76ab5e7b6eac2452793c97c78abf
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-07-29 13:16:51 +0530

    Updated comments to keep track of dependenccies

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
+const morgan = require('morgan') //not added
+const bodyParser = require('body-parser') //not added
 
 //import product routes
 const productRoutes = require('./api/routes/product')
